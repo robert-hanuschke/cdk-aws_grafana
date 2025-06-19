c096dce4f5b91c108f4fb901a7d52bb2cafa7ba7
Robert <robert@hanuschke.eu>
2025-06-19 14:15:50 +0200

    feat: add java

--- .projenrc.ts
+++ .projenrc.ts
@@ -25,6 +25,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
   publishToGo: {
     moduleName: 'github.com/robert-hanuschke/robhan-cdk-lib-aws-grafana',
   },
+  publishToMaven: {
+    javaPackage: 'io.github.roberthanuschke.cdk.aws.grafana',
+    mavenGroupId: 'io.github.robert-hanuschke',
+    mavenArtifactId: 'cdk-aws-grafana',
+  },
   publishToNuget: {
     dotNetNamespace: 'Robhan.CdkLib',
     packageId: 'Robhan.CdkLib.AwsGrafana',
